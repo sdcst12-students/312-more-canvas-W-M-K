c4e49e6d73fbd59ae4aa8825743a257e242f9e60
W-M-K <124245448+W-M-K@users.noreply.github.com>
2023-06-01 11:21:10 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -7,9 +7,35 @@ rectangle object through.
 w = tk.Tk()
 w.geometry("925x475")
 w.attributes('-topmost',True)
-c = tk.Canvas(height=475,width=900,bg="#ffdddd")
+c = tk.Canvas(height=475,width=900,bg="#00EEEE")
 c.pack()
 f = open('map1.txt')
+
+
+rec = c.create_rectangle(20,20,40,40,fill="#006400")
+def keyPress(event):
+    key = event.keysym
+    x1, y1, x2, y2 = c.coords(rec)
+    distance = 10  
+
+    if key == "Left":
+        if x1 - distance >= 0:
+            c.move(rec, -distance, 0)
+    elif key == "Right":
+        if x2 + distance <= c.winfo_width():
+            c.move(rec, distance, 0)
+    elif key == "Up":
+        if y1 - distance >= 0:
+            c.move(rec, 0, -distance)
+    elif key == "Down":
+        if y2 + distance <= c.winfo_height():
+            c.move(rec, 0, distance)
+    
+
+w.bind("<Left>",keyPress)
+w.bind("<Right>",keyPress)
+w.bind("<Up>",keyPress)
+w.bind("<Down>",keyPress)
 
 
 w.mainloop()
